784207fe16b7dae69985f13f926fde550659bdca
Marcelo Shima <marceloshima@gmail.com>
2022-09-19 21:09:55 -0300

    regenerate package.json using jhipster 7.9.3 (#67)

--- generators/server/generator.spec.mjs
+++ generators/server/generator.spec.mjs
@@ -1,13 +1,16 @@
-import expect from 'expect';
+import { expect } from 'expect';
 
 import { helpers, lookups } from '#test-utils';
+
+const SUB_GENERATOR = 'server';
+const BLUEPRINT_NAMESPACE = `jhipster:${SUB_GENERATOR}`;
 
 describe('SubGenerator server of native JHipster blueprint', () => {
   describe('run', () => {
     let result;
     before(async function () {
       result = await helpers
-        .create('jhipster:server')
+        .create(BLUEPRINT_NAMESPACE)
         .withOptions({
           reproducible: true,
           defaults: true,
