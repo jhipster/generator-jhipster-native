4440e78458bbd9192a7100cf2d69962e44142e48
Hideyuki Kagami <hide212131@gmail.com>
2023-08-27 08:46:16 +0900

    update to the latest generate-blueprint, including vitest

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   recursive: true,
-  reporter: 'spec',
+  reporter: "spec",
   slow: 0,
   timeout: 30000,
-  ui: 'bdd',
-  extension: ['spec.js', 'spec.cjs', 'spec.mjs'],
-  exclude: ['**/resources/**', '**/node_modules/**'],
+  ui: "bdd",
+  extension: ["spec.js", "spec.cjs", "spec.mjs"],
+  exclude: ["**/resources/**", "**/node_modules/**"],
   parallel: true,
 };
