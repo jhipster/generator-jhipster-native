7a409b30f40f225b4cae9dd5b2a1d0dff090d7f3
Hideyuki Kagami <hide212131@gmail.com>
2023-08-27 11:47:22 +0900

    update test format (content still to be corrected)

--- generators/app/generator.spec.mjs
+++ generators/app/generator.spec.mjs
@@ -1,17 +1,22 @@
+import { beforeAll, describe, it } from 'vitest';
+
 import { helpers, lookups } from '#test-utils';
+
+const SUB_GENERATOR = 'app';
+const BLUEPRINT_NAMESPACE = `jhipster:${SUB_GENERATOR}`;
 
 describe('SubGenerator app of native JHipster blueprint', () => {
   describe('with defaults config', () => {
     let result;
-    before(async function () {
+    beforeAll(async function () {
       result = await helpers
-        .create('jhipster:app')
+        .create(BLUEPRINT_NAMESPACE)
         .withOptions({
           reproducible: true,
           defaults: true,
+          baseName: 'jhipster',
+          ignoreNeedlesError: true,
           blueprint: 'native',
-          skipClient: true,
-          skipServer: true,
         })
         .withLookups(lookups)
         .run();
@@ -23,15 +28,15 @@ describe('SubGenerator app of native JHipster blueprint', () => {
   });
   describe('with defaults config, foo cacheProvider and true enableHibernateCache', () => {
     let result;
-    before(async function () {
+    beforeAll(async function () {
       result = await helpers
-        .create('jhipster:app')
+        .create(BLUEPRINT_NAMESPACE)
         .withOptions({
           reproducible: true,
           defaults: true,
+          baseName: 'jhipster',
+          ignoreNeedlesError: true,
           blueprint: 'native',
-          skipClient: true,
-          skipServer: true,
           enableHibernateCache: true,
           cacheProvider: 'foo',
         })
