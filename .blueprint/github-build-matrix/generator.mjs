9bf6dd867760d1e5ffeae95d5d8abe7d4e8b5afd
Marcelo Shima <marceloshima@gmail.com>
2024-05-02 11:24:46 -0300

    build matrix

--- .blueprint/github-build-matrix/generator.mjs
+++ .blueprint/github-build-matrix/generator.mjs
@@ -1,8 +1,7 @@
 import { existsSync, appendFileSync } from 'node:fs';
 import os from 'node:os';
-import { readdir } from 'node:fs/promises';
 import BaseGenerator from 'generator-jhipster/generators/base';
-import { defaultMatrix } from './default-matrix.mjs';
+import { buildMatrix } from './build-matrix.mjs';
 
 export default class extends BaseGenerator {
   constructor(args, opts, features) {
@@ -12,11 +11,7 @@ export default class extends BaseGenerator {
   get [BaseGenerator.WRITING]() {
     return this.asWritingTaskGroup({
       async buildMatrix() {
-        const samples = await readdir(this.templatePath('../../generate-sample/templates/samples'));
-        const matrix = {
-          ...defaultMatrix,
-          'sample-name': samples.filter(sample => !sample.includes('disabled')),
-        };
+        const matrix = await buildMatrix(this.templatePath('../../generate-sample/templates/samples'));
         const matrixoutput = `matrix<<EOF${os.EOL}${JSON.stringify(matrix)}${os.EOL}EOF${os.EOL}`;
         const filePath = process.env['GITHUB_OUTPUT'];
         console.log(matrixoutput);
