0ada3c15a1a0a82e05a95229839570f765d13197
Marcelo Shima <marceloshima@gmail.com>
2024-05-03 08:00:17 -0300

    workflow adjusts

--- .blueprint/github-build-matrix/build-matrix.mjs
+++ .blueprint/github-build-matrix/build-matrix.mjs
@@ -2,15 +2,14 @@ import { readdir } from 'fs/promises';
 import { RECOMMENDED_JAVA_VERSION, RECOMMENDED_NODE_VERSION } from 'generator-jhipster';
 import { fromMatrix } from 'generator-jhipster/testing';
 
-import { GRAALVM_VERSION } from '../../lib/constants.js';
-
 const defaultMatrix = {
-  os: ['ubuntu-20.04', 'macos-11', 'windows-2022'],
+  // macos-14 is m1 backed https://docs.github.com/pt/actions/using-github-hosted-runners/about-github-hosted-runners/about-github-hosted-runners
+  // actions-setup-docker currently does not support arm64 https://github.com/docker-practice/actions-setup-docker/issues/38
+  os: ['ubuntu-latest', 'macos-13', 'windows-latest'],
   'build-tool': ['maven', 'gradle'],
   'node-version': [RECOMMENDED_NODE_VERSION],
   'java-version': [RECOMMENDED_JAVA_VERSION],
   'default-environment': ['prod'],
-  'graalvm-version': [GRAALVM_VERSION],
 };
 
 export const buildMatrix = async samplesFolder => {
@@ -23,16 +22,9 @@ export const buildMatrix = async samplesFolder => {
       }),
     ).map(value => ({
       ...value,
-      ...(value.os === 'windows-2022'
-        ? {
-            'default-environment': 'dev',
-            e2e: 'false',
-            'jdl-extra-args': '--skip-install',
-          }
-        : {
-            'default-environment': 'prod',
-            e2e: 'true',
-          }),
+      ...(value.os.startsWith('windows-')
+        ? { 'default-environment': 'dev', e2e: 'false' }
+        : { 'default-environment': 'prod', e2e: 'true' }),
     })),
   };
 };
