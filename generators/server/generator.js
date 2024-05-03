b876ca29f89fb05a8560f2feb8f562e4dd03107a
Marcelo Shima <marceloshima@gmail.com>
2024-05-03 16:10:50 -0300

    Update generator.js

--- generators/server/generator.js
+++ generators/server/generator.js
@@ -271,7 +271,7 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;`,
 
         // Increase wait for macos.
         this.editFile('src/main/docker/keycloak.yml', { assertModified: true }, content =>
-          content.replace('start_period: 10s', 'start_period: 30s').replace('retries: 20', 'retries: 40'),
+          content.replace('start_period: 10s', 'start_period: 30s').replace('retries: 20', 'retries: 60'),
         );
 
         const awaitScript = this.packageJson.getPath('scripts.ci:server:await');
