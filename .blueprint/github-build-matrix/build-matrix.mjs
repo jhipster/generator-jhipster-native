19e10c658e6cd4c709ba08c80e84bf4fbf891a3e
Marcelo Shima <marceloshima@gmail.com>
2024-05-03 13:07:58 -0300

    Update build-matrix.mjs

--- .blueprint/github-build-matrix/build-matrix.mjs
+++ .blueprint/github-build-matrix/build-matrix.mjs
@@ -5,7 +5,7 @@ import { fromMatrix } from 'generator-jhipster/testing';
 const defaultMatrix = {
   // macos-14 is m1 backed https://docs.github.com/pt/actions/using-github-hosted-runners/about-github-hosted-runners/about-github-hosted-runners
   // actions-setup-docker currently does not support arm64 https://github.com/docker-practice/actions-setup-docker/issues/38
-  os: ['ubuntu-latest', 'macos-13', 'windows-latest'],
+  os: ['ubuntu-latest', 'macos-12', 'windows-latest'],
   'build-tool': ['maven', 'gradle'],
   'node-version': [RECOMMENDED_NODE_VERSION],
   'java-version': [RECOMMENDED_JAVA_VERSION],
