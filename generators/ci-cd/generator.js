a80c04ba3a754392bce3579c1009a1c5ac3c0932
Marcelo Shima <marceloshima@gmail.com>
2024-05-07 13:54:53 -0300

    bump actions

--- generators/ci-cd/generator.js
+++ generators/ci-cd/generator.js
@@ -2,11 +2,11 @@ import CiCdGenerator from 'generator-jhipster/generators/base-application';
 import { RECOMMENDED_NODE_VERSION, RECOMMENDED_JAVA_VERSION } from 'generator-jhipster';
 
 const githubActions = {
-  'actions/checkout': 'actions/checkout@v3',
-  'actions/cache': 'actions/cache@v3',
-  'actions/upload-artifact': 'actions/upload-artifact@v3',
+  'actions/checkout': 'actions/checkout@v4',
+  'actions/cache': 'actions/cache@v4',
+  'actions/upload-artifact': 'actions/upload-artifact@v4',
   'graalvm/setup-graalvm': 'graalvm/setup-graalvm@v1',
-  'actions/setup-node': 'actions/setup-node@v3',
+  'actions/setup-node': 'actions/setup-node@v4',
 };
 
 export default class extends CiCdGenerator {
