f9c1bf8f4811dc91f5e3db2da20f5a39936be55e
Marcelo Boveto Shima <marceloshima@gmail.com>
2024-06-03 17:12:53 -0300

    adjust keycloak docker file

--- generators/server/generator.js
+++ generators/server/generator.js
@@ -314,9 +314,7 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;`,
         if (!application.authenticationTypeOauth2) return;
 
         // Increase wait for macos. Keyclock container start can take over 3 min. 4 min is not enough to download/start containers/start server.
-        this.editFile('src/main/docker/keycloak.yml', { assertModified: true }, content =>
-          content.replace('start_period: 10s', 'start_period: 30s').replace('retries: 20', 'retries: 60'),
-        );
+        this.editFile('src/main/docker/keycloak.yml', { assertModified: true }, content => content.replace('retries: 20', 'retries: 100'));
 
         const awaitScript = this.packageJson.getPath('scripts.ci:server:await');
         if (awaitScript) {
