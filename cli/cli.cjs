780d2d21b7bddc03ff841a42dab7fd3e1664e551
Marcelo Shima <marceloshima@gmail.com>
2024-09-23 18:44:22 -0300

    Update cli.cjs

--- cli/cli.cjs
+++ cli/cli.cjs
@@ -23,7 +23,7 @@ const blueprint = packageFolderName.startsWith('jhipster-') ? `generator-${packa
       [blueprint]: version,
     },
     printBlueprintLogo: () => {
-      console.log('===================== JHipster native =====================');
+      console.log('===================== JHipster Native =====================');
       console.log('');
     },
     lookups: [{ packagePaths: [packagePath] }],
