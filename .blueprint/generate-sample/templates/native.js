6b88ffb34cbfbb9e3a31eeb142ba7bb33d9593ec
Marcelo Shima <marceloshima@gmail.com>
2026-07-20 23:00:50 -0300

    chore: update workflow

--- .blueprint/generate-sample/templates/native.js
+++ .blueprint/generate-sample/templates/native.js
@@ -16,7 +16,7 @@ export default Object.fromEntries(
     {
       ...spec,
       ...(!spec.os.startsWith('ubuntu-') ? { 'default-environment': 'dev', e2e: 'false' } : { 'default-environment': 'prod', e2e: 'true' }),
-      'java-version': '21',
+      'java-version': '25',
     },
   ]),
 );
